ade9f55a15a14f8627d9c66c6b3fc53d21f2a118
pinkswall <tmddn741741@gmail.com>
2020-10-23 23:09:17 +0900

    주석 추가, startTime 형식 변경
    @return, @ request_count 추가
    
    startTime 형식을 EPG XML에 바로 쓸 수 있도록 수정

--- GetEPG/FromLGU.py
+++ GetEPG/FromLGU.py
@@ -7,20 +7,32 @@ import re
 
 def GetEPGFromLGU(serviceId: str, period: int) -> List:
     """
-    LGU에서 ServiceId에 해당하는 채널의 EPG를 받아옵니다.
-    TODO: 회차 정보 담기
-    TODO: 주석 업데이트
+    LGU에서 ServiceId에 해당하는 채널의 EPG를 받아옵니다. \n
+    @return [
+        {
+            'programTitle': '프로그램 이름',
+            'subtitle': '부제목',
+            'category': '카테고리',
+            'startTime': 'HH:MM',
+            'isRebroadcast': True | False,
+            'KMRB': '전체관람가' | '12세이상관람가' | '15세이상관람가' | '청소년관람불가'
+        }
+    ] \n
+    @request_count: period \n
+    TODO: 회차 정보 담기\n
+    TODO: startTime형식 HH:MM -> YYYYMMDDhhmmss으로 변경
     """
 
     URL = 'http://www.uplus.co.kr/css/chgi/chgi/RetrieveTvSchedule.hpi'
     UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'
 
-    result = []
     p_fullTitle = re.compile(r'.+(?=\n)')
     p_title = re.compile(r'^[^[|^(^<]+')
     p_subtitle = re.compile(r'(?<=\[).+(?=\])')
     p_rebroadcast = re.compile(r'<재>')
+
     today = date.today()
+    result = []
     for day in range(period):
         target_day = today + timedelta(days=day)
         req = requests.post(URL, params={'chnlCd': serviceId, 'evntCmpYmd': target_day.strftime('%Y%m%d')}, headers={'User-Agent': UA})
@@ -37,10 +49,10 @@ def GetEPGFromLGU(serviceId: str, period: int) -> List:
             is_rebroadcast = True if p_rebroadcast.search(programFullTitle) else False
 
             result.append({
-                'startTime': channel.find(attrs={'class': 'txtC'}).string.strip(),
-                'programName': programTitle,
+                'programTitle': programTitle,
                 'subtitle': subtitle,
                 'category': channel.find(attrs={'class': 'txtC hidden-xs'}).string.strip(),
+                'startTime': datetime.strptime(str(target_day) + ' ' + channel.find(attrs={'class': 'txtC'}).string.strip(), '%Y-%m-%d %H:%M').strftime('%Y%m%d%H%M%S'),
                 'isRebroadcast': is_rebroadcast,
                 'KMRB': KMRB
             })
