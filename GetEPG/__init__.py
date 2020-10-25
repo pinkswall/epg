2b1b3ea25ff0ec6fd67c532df5c5b6e3bb78a5e2
pinkswall <tmddn741741@gmail.com>
2020-10-25 22:44:20 +0900

    Update __init__.py
    네이버 함수 추가

--- GetEPG/__init__.py
+++ GetEPG/__init__.py
@@ -1,6 +1,7 @@
-""" df """
+""" EPG를 받아옵니다. """
 from .FromKT import GetEPGFromKT
 from .FromLGU import GetEPGFromLGU
 from .FromSKB import GetEPGFromSKB
+from .FromNAVER import GetEPGFromNAVER
 from .FromTVING import GetEPGFromTVING
 from .FromWAVVE import GetEPGFromWAVVE
