3979a7ab7ec76794592c7229a8afc9092a1811d0
pinkswall <tmddn741741@gmail.com>
2020-10-25 22:47:33 +0900

    CamelCase

--- GenerateXML/writeChannel.py
+++ GenerateXML/writeChannel.py
@@ -1,22 +1,22 @@
-from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
+from xml.etree.ElementTree import Element, SubElement, tostring
 from typing import Dict
 
-def writeChannel(channelInfo: Dict):
+def writeChannel(channelInfo: Dict) -> str:
     """
     채널 정보를 입력받아 xml형식으로 정리합니다. \n
     @param channelInfo - \n
     {
-        'id': '채널ID',
-        'name': '채널이름',
-        'icon': 'https://ddns/path/to/icon'
+        'Id': '채널ID',
+        'Name': '채널이름',
+        'Icon_url': 'https://ddns/path/to/icon'
     } \n
     @return xml string
     """
 
-    channel = Element('channel', id=channelInfo['id'])
+    channel = Element('channel', id=channelInfo['Id'])
 
-    SubElement(channel, 'display-name').text = channelInfo['name']
+    SubElement(channel, 'display-name').text = channelInfo['Name']
 
-    SubElement(channel, 'icon', attrib={'src': channelInfo['icon']})
+    SubElement(channel, 'icon', attrib={'src': channelInfo['Icon_url']})
 
     return tostring(channel, encoding='unicode')
