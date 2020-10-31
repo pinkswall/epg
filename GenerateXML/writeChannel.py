b78d41c4e6c94444f414661a778af7577cd0656a
pinkswall <tmddn741741@gmail.com>
2020-10-31 16:16:14 +0900

    Id가 없으면 임의로 지정

--- GenerateXML/writeChannel.py
+++ GenerateXML/writeChannel.py
@@ -6,17 +6,19 @@ def writeChannel(channelInfo: Dict) -> str:
     채널 정보를 입력받아 xml형식으로 정리합니다. \n
     @param channelInfo - \n
     {
-        'Id': '채널ID',
+        'Id'?: '채널ID',
         'Name': '채널이름',
-        'Icon_url': 'https://ddns/path/to/icon'
+        'Icon_url'?: 'https://ddns/path/to/icon'
     } \n
     @return xml string
     """
-
+    if 'Id' not in channelInfo:
+        channelInfo['Id'] = channelInfo['Name']
+        
     channel = Element('channel', id=channelInfo['Id'])
 
     SubElement(channel, 'display-name').text = channelInfo['Name']
 
-    SubElement(channel, 'icon', attrib={'src': channelInfo['Icon_url']})
+    if 'Icon_url' in channelInfo: SubElement(channel, 'icon', attrib={'src': channelInfo['Icon_url']})
 
     return tostring(channel, encoding='unicode')
