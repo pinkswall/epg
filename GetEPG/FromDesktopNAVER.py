e3c7d42edaa2100eb749b588ab82b285f4d9b6a9
pinkswall <tmddn741741@gmail.com>
2020-10-31 16:14:22 +0900

    Update FromDesktopNAVER.py

--- GetEPG/FromDesktopNAVER.py
+++ GetEPG/FromDesktopNAVER.py
@@ -18,6 +18,7 @@ def GetEPGFromDesktopNAVER(Query: str) -> List:
       }
   ] \n
   """
+  
   URL = "https://search.naver.com/search.naver" + Query
   UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
   req = requests.get(URL, headers={'User-Agent': UA})
