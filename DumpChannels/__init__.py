8fbdfc41a100a48f95ea9c67e6381e3ffe8485b7
pinkswall <tmddn741741@gmail.com>
2020-11-01 21:21:46 +0900

    __init__에서 import문 제거
    절대경로로 사용하십시오

--- DumpChannels/__init__.py
+++ DumpChannels/__init__.py
@@ -1,6 +1,2 @@
 """ 각 소스의 채널 목록을 리턴합니다. """
-from .FromLGU import DumpChannelsFromLGU
-from .FromNAVER import DumpChannelsFromNAVER
-from .FromSKB import DumpChannelsFromSKB
-from .FromTVING import DumpChannelsFromTVING
-from .FromWAVVE import DumpChannelsFromWAVVE
+
--- GenerateXML/__init__.py
+++ GenerateXML/__init__.py
@@ -1,3 +1,1 @@
 """ 채널이나 프로그램 정보를 입력받아 XML형식으로 반환합니다. """
-from .writeChannel import writeChannel
-from .writeProgram import writeProgram
